4c75f63a5d6695b2ca0fbf7ab58f1effb95b10ae
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-21 00:18:05 +0530

    commit

--- src/main/java/com/example/jwt/config/Configaration.java
+++ src/main/java/com/example/jwt/config/Configaration.java
@@ -2,6 +2,8 @@ package com.example.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,16 +17,22 @@ public class Configaration {
     @Bean
     public PasswordEncoder passwordEncoder() {
 
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // Password Encoder Using BcryptPassword
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
+        /*
+        * This is for the Admin User Configuration
+        */
         UserDetails adminUser = User.builder()
                 .username("Admin")
                 .password("Admin")
                 .roles("ADMIN").build();
 
+        /*
+         * This is for Normal User Configuration
+         */
         UserDetails user = User.builder()
                 .username("User")
                 .password("User")
@@ -32,6 +40,17 @@ public class Configaration {
 
         return new InMemoryUserDetailsManager(adminUser, user); // This is for the Save In memory
                                                                 // user Save..
+    }
+
+    /*
+     * This is for Authentication Manager Configuration Class
+     * For User Authentication.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
 }
